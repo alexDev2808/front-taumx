430ffe78129aca148162054b3ace98b87a5b7e32
J Alexis <colores.2808.8000@gmail.com>
2026-01-22 10:48:25 -0600

    fix: fix breadcrumb separator

--- resources/views/layouts/partials/breadcrumbs.blade.php
+++ resources/views/layouts/partials/breadcrumbs.blade.php
@@ -5,7 +5,7 @@
         <ol class="flex flex-wrap">
             @foreach ($breadcrumbs as $breadcrumb)
                 <li 
-                    @class(['ps-2 before:float-left before:pe-2 before:content-[\'/\']' => !$loop->first, 'text-sm leading-normal text-slate-600'])>
+                    @class(["ps-2 before:float-left before:pe-2 before:content-['>']" => !$loop->first, 'text-sm leading-normal text-slate-600'])>
 
                     @isset($breadcrumb['route'])   
                         <a class="opacity-50" href="{{ $breadcrumb['route'] }}">{{ $breadcrumb['name']}}</a>
